7447225db93c3e4be5cc9ec40d1b010bf9f0cb2b
Paul <paul.gladoon@gmail.com>
2017-09-01 21:13:47 +0300

    add homeTask12

--- specs/homeTask11.js
+++ specs/homeTask11.js
@@ -4,7 +4,7 @@ require('events').EventEmitter.defaultMaxListeners = 0; // shutdown max listener
 var EC = protractor.ExpectedConditions; // assert protractor expected conditions
 
 
-describe('Home task #10', function () {
+describe('Home task #11', function () {
 
     beforeEach(function () {
         browser.ignoreSynchronization = true; // ignore Angular
---
+++ specs/homeTask12.js
@@ -0,0 +1,81 @@
+var properties = require('../resoursces/properties.js');
+var authorization = require('../app-manager/authorization-helper.js');
+
+require('events').EventEmitter.defaultMaxListeners = 0; // shutdown max listeners
+var EC = protractor.ExpectedConditions; // assert protractor expected conditions
+
+
+describe('Home task #12', function () {
+
+    beforeEach(function () {
+        browser.ignoreSynchronization = true; // ignore Angular
+        browser.driver.manage().window().setSize(properties.width, properties.height); // window resolution
+        browser.get('http://localhost:8080/litecart/admin/');
+
+    });
+
+    it('Admin add product', function () {
+        // prepare
+        var randomProductName = 'TestProduct'+Date.now();
+        var randomProductCode = Date.now();
+
+        // act
+        authorization.login(properties.userName, properties.userPassword);
+
+        element(by.css('[href*=catalog]')).click();
+        element(by.css('[href*=edit_product]')).click();
+
+        // general
+        element(by.css('[value="1"][name=status]')).click();
+        element(by.css('[name="name[en]"]')).sendKeys(randomProductName);
+        element(by.name('code')).sendKeys(randomProductCode);
+        element(by.css('[name="product_groups[]"][value="1-3"]')).click();
+        element(by.name('quantity')).clear().sendKeys('100');
+        element(by.name("new_images[]")).sendKeys('/Users/pavelgladoon/Documents/Selenium_courses/resoursces/guitar.JPG');
+
+        var dateValidFromSelect = element(by.name('date_valid_from'));
+        var dateValidToSelect = element(by.name('date_valid_to'));
+
+        browser.actions()
+                .mouseMove(dateValidFromSelect, {x: 10, y: 0})
+                .click()
+                .sendKeys('08252017')
+                .perform();
+
+        browser.actions()
+                .mouseMove(dateValidToSelect, {x: 10, y: 0})
+                .click()
+                .sendKeys('11252017')
+                .perform();
+
+        element(by.css('[href*=information]')).click();
+        browser.sleep(1000);
+
+        // information
+        element(by.name('manufacturer_id')).click();
+        element(by.css('[name=manufacturer_id] > [value="1"]')).click();
+        element(by.name('keywords')).sendKeys('Guitar');
+        element(by.css('[name="short_description[en]"]')).sendKeys('Best Guitar');
+        element(by.css('.trumbowyg-editor')).click().sendKeys('Good Guitar');
+        element(by.css('[name="head_title[en]"]')).sendKeys('Guitar Title');
+        element(by.css('[name="meta_description[en]"]')).sendKeys('Guitar Meta Description');
+
+        element(by.css('[href*=prices]')).click();
+        browser.sleep(1000);
+
+        // prices
+        element(by.name('purchase_price')).clear().sendKeys('200');
+        element(by.name('purchase_price_currency_code')).click();
+        element(by.css('[name=purchase_price_currency_code] > [value="USD"]')).click();
+        element(by.css('[name="gross_prices[USD]"]')).clear().sendKeys('120');
+        element(by.css('[name="gross_prices[EUR]"]')).clear().sendKeys('90');
+        element(by.name('save')).click();
+
+
+        // assert
+        var createdProduct = element(by.cssContainingText('a', randomProductName));
+        browser.wait(EC.visibilityOf(createdProduct), 5000);
+
+    });
+
+});
